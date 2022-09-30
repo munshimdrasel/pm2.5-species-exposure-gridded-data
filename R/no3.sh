a33da924d135de8162e2d64a27c3486980744370
munshimdrasel <munshimdrasel@gwu.edu>
2022-09-30 11:24:48 -0400

    -gridded no3 data to block group no3 data -slurm script

---
+++ R/no3.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --job-name=pm.species.no3
+#SBATCH --partition=normal
+#SBATCH --output=/scratch/%u/%x-%N-%j.out  # Output file
+#SBATCH --error=/scratch/%u/%x-%N-%j.err   # Error file
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=mrasel@gmu.edu
+#SBATCH --mem-per-cpu=2GB
+#SBATCH --time=2-00:00:00
+#SBATCH --ntasks=32
+#SBATCH --nodes=2
+
+cd /projects/HAQ_LAB/mrasel/R/pm2.5-species-exposure-gridded-data/R
+
+
+module load r-disperseR/0.1.0  
+
+Rscript --no-restore --quiet --no-save no3.R
+
+
